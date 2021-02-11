d5b2b79792bf5f8e7594bf878aaa2b067d68fb05
BRvanHogen <brvanhogen@hotmail.nl>
2021-02-11 19:33:52 +0100

    did the first setup: installed axios and linked all files

---
+++ main.js
@@ -0,0 +1,7 @@
+const clickedButton = document.getElementById("search-button");
+
+clickedButton.addEventListener("click", generateUselessFacts);
+
+async function generateUselessFacts(){
+    console.log("Constantijn!");
+}
