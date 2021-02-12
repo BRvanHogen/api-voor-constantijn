64f412541cca67a60d482779f84c8c2983ebd03c
BRvanHogen <brvanhogen@hotmail.nl>
2021-02-12 12:53:41 +0100

    "fixed typo in if-statement and fixed bug in currencies.length which should have been ===1 and not ===0"

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 async function generateUselessFacts() {
     const response = await axios.get(
-        "https://restcountries.eu/rest/v2/name/cuba?fullText=true");
+        "https://restcountries.eu/rest/v2/name/aruba?fullText=true");
     console.log("dit is de hele response", response);
     console.log(response.data[0].name + " is situated in", response.data[0].subregion + "." +
         " It has a population of " + response.data[0].population + " people. The capital is " +
         response.data[0].capital);
 
-        if (response.data[0].currencies.length===0) {
-            console.log("and you can pay with" + response.data[0].currencies[0].name); }
+        if (response.data[0].currencies.length===1) {
+            console.log("and you can pay with " + response.data[0].currencies[0].name); }
         else {
             console.log("and you can pay with " + response.data[0].currencies[0].name
                 + " or " + response.data[0].currencies[1].name);
