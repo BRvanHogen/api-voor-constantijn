a97968233346a56f25a025e41779954e91c8ac47
BRvanHogen <brvanhogen@hotmail.nl>
2021-02-12 12:43:54 +0100

    "added function that checks valid currencies and prints string to console depending on the amount of valid currencies"

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-const clickedButton = document.getElementById("search-button");
+async function generateUselessFacts() {
+    const response = await axios.get(
+        "https://restcountries.eu/rest/v2/name/cuba?fullText=true");
+    console.log("dit is de hele response", response);
+    console.log(response.data[0].name + " is situated in", response.data[0].subregion + "." +
+        " It has a population of " + response.data[0].population + " people. The capital is " +
+        response.data[0].capital);
 
-clickedButton.addEventListener("click", generateUselessFacts);
-
-async function generateUselessFacts(){
-    console.log("Constantijn!");
+        if (response.data[0].currencies.length===0) {
+            console.log("and you can pay with" + response.data[0].currencies[0].name); }
+        else {
+            console.log("and you can pay with " + response.data[0].currencies[0].name
+                + " or " + response.data[0].currencies[1].name);
+        }
 }
+
+    const clickedButton = document.getElementById("search-button");
+    clickedButton.addEventListener("click", generateUselessFacts);
+
+
+
+
+
+
+
+// const url = "https://restcountries.eu/rest/v2/name/${country}?fullText=true";
+// axios.get(url);
+
+
+
+
