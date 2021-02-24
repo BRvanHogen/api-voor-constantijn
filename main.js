bc729986d9c88d6904bf0f5f8795a08c7160eb72
BRvanHogen <brvanhogen@hotmail.nl>
2021-02-24 16:18:08 +0100

    Removed the aforementioned function which was commented out anyway.

--- main.js
+++ main.js
@@ -6,17 +6,6 @@ async function generateUselessFacts() {
     const response = await axios.get(
         api + userInput);
     console.log("dit is de hele response", response);
-    // console.log(response.data[0].name + " is situated in", response.data[0].subregion + "." +
-    //     " It has a population of " + response.data[0].population + " people. The capital is " +
-    //     response.data[0].capital);
-    //
-    //
-    //     if (response.data[0].currencies.length === 1) {
-    //         console.log("and you can pay with " + response.data[0].currencies[0].name);
-    //     } else {
-    //         console.log("and you can pay with " + response.data[0].currencies[0].name
-    //             + " or " + response.data[0].currencies[1].name);
-    //     }
 
     // tussen deze regel
     imageUrl = response.data[0].flag;
